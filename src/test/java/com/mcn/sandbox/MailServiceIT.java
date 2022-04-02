c33d33981e0104da8ff8af646b7d349065315986
Keith McNeill <k.mcneill@blackarrowgroup.io>
2022-04-02 21:25:18 +0100

    Ordered build command reports (2)

---
+++ src/test/java/com/mcn/sandbox/MailServiceIT.java
@@ -0,0 +1,14 @@
+package com.mcn.sandbox;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class MailServiceIT {
+
+	@Test
+	void contextLoads() {
+		assertTrue(true);
+	}
+
+}
---
+++ src/test/java/com/mcn/sandbox/service/MailServiceTest.java
@@ -0,0 +1,12 @@
+package com.mcn.sandbox.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class MailServiceTest {
+
+    @Test
+    void testGetMail() {
+        Assertions.assertEquals("Mail", "Mail");
+    }
+}
