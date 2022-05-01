e2ce19d10ce77a7ee01731d16db5bcf04b6c7012
Keith McNeill <k.mcneill@blackarrowgroup.io>
2022-05-01 20:36:32 +0100

    Run SQS test container.

--- src/test/java/com/github/sparsick/testcontainerspringboot/hero/containers/HeroContainersJpaRepositoryIT.java
+++ src/test/java/com/github/sparsick/testcontainerspringboot/hero/containers/HeroContainersJpaRepositoryIT.java
@@ -1,29 +1,72 @@
 package com.github.sparsick.testcontainerspringboot.hero.containers;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.sqs.AmazonSQSAsync;
+import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
+import com.amazonaws.services.sqs.model.CreateQueueResult;
 import com.github.sparsick.testcontainerspringboot.hero.universum.ComicUniversum;
 import com.github.sparsick.testcontainerspringboot.hero.universum.Hero;
 import com.github.sparsick.testcontainerspringboot.hero.universum.HeroClassicJpaRepository;
-import org.junit.jupiter.api.Disabled;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.util.Collection;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.hasSize;
 
 class HeroContainersJpaRepositoryIT extends TestContainersBaseTest {
 
     @Autowired
     private HeroClassicJpaRepository repositoryUnderTest;
 
+    @Value("${aws.sqs.endpoint}")
+    private String endpoint;
+
+    @Value("${aws.credentials.access-key}")
+    String accessKey;
+
+    @Value("${aws.credentials.secret-key}")
+    String secretKey;
+
+    private AmazonSQSAsync client;
+
+    @BeforeEach
+    void setUp() {
+        client = getClient();
+    }
+
     @Test
-    void findAllHero(){
+    void testCreateQueue() {
+        CreateQueueResult result = client.createQueue("myqueue1");
+
+        assertThat(result.getQueueUrl(), Matchers.endsWith("myqueue1"));
+    }
+
+    private AmazonSQSAsync getClient() {
+        AmazonSQSAsync sqsAsync = AmazonSQSAsyncClientBuilder.standard()
+                .withCredentials(getCredentialsProvider())
+                .withEndpointConfiguration(
+                        new AwsClientBuilder.EndpointConfiguration(
+                                endpoint, Regions.US_EAST_2.getName()
+                        )
+                ).build();
+        return sqsAsync;
+    }
+
+    private AWSStaticCredentialsProvider getCredentialsProvider() {
+        return new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey));
+    }
+
+    @Test
+    void findAllHero() {
         int numberHeroes = repositoryUnderTest.allHeroes().size();
 
         repositoryUnderTest.addHero(new Hero("Batman", "Gotham City", ComicUniversum.DC_COMICS));
@@ -31,16 +74,16 @@ class HeroContainersJpaRepositoryIT extends TestContainersBaseTest {
 
         Collection<Hero> heros = repositoryUnderTest.allHeroes();
 
-        assertThat(heros).hasSize(numberHeroes + 2);
+        assertThat(heros, hasSize(numberHeroes + 2));
     }
 
     @Test
-    void findHeroByCriteria(){
+    void findHeroByCriteria() {
         repositoryUnderTest.addHero(new Hero("Batman", "Gotham City", ComicUniversum.DC_COMICS));
 
         Collection<Hero> heros = repositoryUnderTest.findHerosBySearchCriteria("Batman");
 
-        assertThat(heros).contains(new Hero("Batman", "Gotham City", ComicUniversum.DC_COMICS));
+        assertThat(heros, contains(new Hero("Batman", "Gotham City", ComicUniversum.DC_COMICS)));
     }
 
 }
--- src/test/java/com/github/sparsick/testcontainerspringboot/hero/containers/TestContainersBaseTest.java
+++ src/test/java/com/github/sparsick/testcontainerspringboot/hero/containers/TestContainersBaseTest.java
@@ -1,5 +1,7 @@
 package com.github.sparsick.testcontainerspringboot.hero.containers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -7,39 +9,53 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.containers.Network;
+import org.testcontainers.containers.localstack.LocalStackContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+import static org.testcontainers.containers.localstack.LocalStackContainer.Service.S3;
+import static org.testcontainers.containers.localstack.LocalStackContainer.Service.SQS;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @DirtiesContext
 @Testcontainers
 public abstract class TestContainersBaseTest {
-
-//    private static Network network = Network.newNetwork();
-//    @ClassRule
-//    public static LocalStackContainer localstackInDockerNetwork = new LocalStackContainer()
-//            .withNetwork(network)
-//            .withNetworkAliases("notthis", "localstack")    // the last alias is used for HOSTNAME_EXTERNAL
-//            .withServices(S3, SQS, CLOUDWATCHLOGS);
+    private static final Logger LOGGER = LoggerFactory.getLogger(TestContainersBaseTest.class);
 
     @Autowired
     protected TestRestTemplate testRestTemplate;
 
+    private static Network network = Network.newNetwork();
+
     @Container
-    public static MySQLContainer database =  new MySQLContainer<>("mysql:8.0.16")
+    public static MySQLContainer database = new MySQLContainer<>("mysql:8.0.16")
             .withDatabaseName("test")
             .withUsername("user")
             .withPassword("pass");
-            //.withInitScript("");
 
+    /* Don't use @Container, as it will be re-started before every test class, and it takes ages.  */
+    public static LocalStackContainer localStack = new LocalStackContainer(DockerImageName.parse("localstack/localstack:0.11.3"))
+            .withNetwork(network)
+            .withNetworkAliases("something", "awslocal")
+            .withServices(SQS);
+
+    static {
+        /* Using static block instead, as it will only be started once, thereby speeding up time for multiple tests. */
+        localStack.start();
+    }
 
     @DynamicPropertySource
     public static void properties(DynamicPropertyRegistry registry) {
+        registry.add("aws.sqs.endpoint", () -> localStack.getEndpointOverride(SQS));
+        registry.add("aws.s3.endpoint", () -> localStack.getEndpointOverride(S3));
+        registry.add("aws.credentials.access-key", localStack::getAccessKey);
+        registry.add("aws.credentials.secret-key", localStack::getSecretKey);
         registry.add("spring.datasource.url", database::getJdbcUrl);
         registry.add("spring.datasource.username", database::getUsername);
         registry.add("spring.datasource.password", database::getPassword);
 
     }
-
 
 }
