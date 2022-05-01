bb2f82349158aaaad200b832f55225748c383b0c
Keith McNeill <k.mcneill@blackarrowgroup.io>
2022-05-01 11:12:18 +0100

    Dummy Unit Test for report file

---
+++ src/test/java/com/github/sparsick/testcontainerspringboot/hero/universum/DummyUnitTest.java
@@ -0,0 +1,14 @@
+package com.github.sparsick.testcontainerspringboot.hero.universum;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DummyUnitTest {
+
+    @Test
+    void createDummyReportFileTest() {
+        assertTrue(true);
+    }
+
+}
