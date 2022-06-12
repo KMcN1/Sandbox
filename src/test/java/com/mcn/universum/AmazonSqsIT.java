fce6154f5c558e7394c0107f7232f5ad7f2be763
Keith McNeill <k.mcneill@blackarrowgroup.io>
2022-06-12 17:59:28 +0100

    Separated tests and added missing DynamoDb properties to default YAML

---
+++ src/test/java/com/mcn/universum/AmazonSqsIT.java
@@ -0,0 +1,42 @@
+package com.mcn.universum;
+
+import com.amazonaws.services.sqs.AmazonSQSAsync;
+import com.amazonaws.services.sqs.model.CreateQueueResult;
+import com.amazonaws.services.sqs.model.DeleteQueueResult;
+import com.mcn.containers.IntegrationTestBase;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.jdbc.Sql;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+class AmazonSqsIT extends IntegrationTestBase {
+
+    @Autowired
+    private AmazonSQSAsync sqsAsync;
+
+    private CreateQueueResult result;
+
+    @AfterEach
+    void tearDown(){
+        DeleteQueueResult deleteQueueResult = sqsAsync.deleteQueue(result.getQueueUrl());
+
+        assertThat(deleteQueueResult.getSdkHttpMetadata().getHttpStatusCode(), equalTo(HttpStatus.OK.value()));
+    }
+
+    @Test
+    void testCreateQueue() {
+        result = sqsAsync.createQueue("myqueue1");
+
+
+        assertThat(result.getSdkHttpMetadata().getHttpStatusCode(), equalTo(HttpStatus.OK.value()));
+        assertThat(result.getQueueUrl(), Matchers.endsWith("myqueue1"));
+
+    }
+
+
+}
--- src/test/java/com/mcn/universum/DynamoDbIT.java
+++ src/test/java/com/mcn/universum/DynamoDbIT.java
@@ -1,15 +1,11 @@
 package com.mcn.universum;
 
-import com.amazonaws.services.sqs.AmazonSQSAsync;
-import com.amazonaws.services.sqs.model.CreateQueueResult;
 import com.mcn.containers.IntegrationTestBase;
 import com.mcn.containers.config.DynamoDbTestUtils;
 import com.mcn.ddb.Customer;
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.jdbc.Sql;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
 import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
@@ -23,11 +19,8 @@ import java.util.List;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
-@Sql(scripts = {"classpath:db/clear_data.sql"})
-class HeroContainersJpaRepositoryIT extends IntegrationTestBase {
+class DynamoDbIT extends IntegrationTestBase {
 
-    @Autowired
-    private AmazonSQSAsync sqsAsync;
 
     @Autowired
     private DynamoDbEnhancedClient enhancedClient;
@@ -55,13 +48,4 @@ class HeroContainersJpaRepositoryIT extends IntegrationTestBase {
 
         assertThat(customers, equalTo(Collections.emptyList()));
     }
-
-    @Test
-    void testCreateQueue() {
-        CreateQueueResult result = sqsAsync.createQueue("myqueue1");
-
-        assertThat(result.getQueueUrl(), Matchers.endsWith("myqueue1"));
-    }
-
-
 }
